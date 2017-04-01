8aab29b942e93ba261df19c4480b39911c459274
Alla Kraynova <aukra25@yandex.ru>
2017-04-01 16:46:53 +0300

    gulp

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # iamhero
+
+запуск сборки для разработки npm start
+запуск сборки билда npm run build
---
+++ gulpfile.js
@@ -0,0 +1,114 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var mqpacker = require('css-mqpacker');
+var cssmin = require('gulp-csso');
+var server = require('browser-sync').create();
+var pagebuilder = require('gulp-pagebuilder');
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+var run = require('gulp-sequence');
+var del = require('del');
+var concat = require('gulp-concat');
+var sourcemap = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+
+gulp.task('style', function() {
+  gulp.src('scss/style.scss')
+    .pipe(plumber())
+    .pipe(sourcemap.init({largeFile: true}))
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        'last 2 versions'
+      ]}),
+      mqpacker({
+        sort: false
+      })
+    ]))
+    .pipe(sourcemap.write())
+    .pipe(gulp.dest('build/css'))
+    .pipe(cssmin())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css'))
+    .pipe(server.stream());
+});
+
+gulp.task('html', function () {
+  gulp.src('html/*.html')
+    .pipe(pagebuilder())
+    .pipe(gulp.dest('build'))
+    .pipe(server.reload({stream: true}));
+});
+
+gulp.task('images', function () {
+  return gulp.src('images/**/*.{jpg, png, gif}')
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest('./build/images'));
+});
+
+gulp.task('svgsprite', function () {
+  return gulp.src('images/svg/*.svg')
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/images'));
+});
+
+gulp.task('scripts', function () {
+  gulp.src(['./js/modules/*.js', './js/scripts.js'])
+    .pipe(plumber())
+    .pipe(sourcemap.init({largeFile: true}))
+    .pipe(concat('main.js', {newLine: ';'}))
+    .pipe(sourcemap.write())
+    .pipe(gulp.dest('./build/js'))
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest('./build/js'))
+    .pipe(server.reload({stream: true}));
+});
+
+gulp.task('copy', function () {
+  return gulp.src([
+    'fonts/**/*',
+    'js/vendors/*'
+    ],
+    {
+    base: '.'
+    })
+    .pipe(gulp.dest('build'
+  ));
+});
+
+gulp.task('clean', function () {
+  return del('build');
+});
+
+gulp.task('serve', function() {
+  server.init({
+    server: 'build',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch('scss/**/*.{scss,sass}', ['style']);
+  gulp.watch('js/**/*.js', ['scripts']);
+  gulp.watch('html/**/*.html', ['html']);
+});
+
+gulp.task('build', function(fn) {
+  run('clean', 'copy', 'html', 'style', 'scripts', 'images', 'svgsprite', fn);
+});
