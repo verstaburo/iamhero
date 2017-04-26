d1eabfbed6861c58e3c17ba1b9fffab4804a02a0
Alla Kraynova <aukra25@yandex.ru>
2017-04-26 14:22:41 +0300

    top menu fixed on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -169,3 +169,36 @@ $(function() {
     });
 
 });
+
+//fixed top menu
+
+var StickyElement = function(node){
+  var doc = $(document),
+      fixed = false,
+      anchor = node.find('.sticky__anchor'),
+      content = node.find('.sticky__content');
+
+  var onScroll = function(e){
+    var docTop = doc.scrollTop(),
+        anchorTop = anchor.offset().top;
+
+    console.log('scroll', docTop, anchorTop);
+    if(docTop > anchorTop){
+      if(!fixed){
+        anchor.height(content.outerHeight());
+        content.addClass('fixed');
+        fixed = true;
+      }
+    }  else   {
+      if(fixed){
+        anchor.height(0);
+        content.removeClass('fixed');
+        fixed = false;
+      }
+    }
+  };
+
+  $(window).on('scroll', onScroll);
+};
+
+var demo = new StickyElement($('.sticky'));
