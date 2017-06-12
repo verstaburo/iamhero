af78aca163747307b5169377cce3bae146e514f2
nick_437 <nick_437@mail.ru>
2017-06-12 18:43:06 +0300

    edits video + img

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-document.createElement( "picture" );
+document.createElement("picture");
 svg4everybody({polyfill: true});
 
 
 //radio buttons in form_prereg
 document.onload = (function () {
   var radioElems = document.querySelectorAll('.switch__radio');
-  for(var i = 0; i < radioElems.length; i++) {
-    if(radioElems[i].checked) {
+  for (var i = 0; i < radioElems.length; i++) {
+    if (radioElems[i].checked) {
       var elem = radioElems[i];
-      while(elem) {
+      while (elem) {
         if (elem.parentNode.classList.contains('switch__item')) {
-            elem.parentNode.classList.add('js-radio-checked');
-            break;
+          elem.parentNode.classList.add('js-radio-checked');
+          break;
         } else {
           elem = elem.parentNode;
         }
@@ -20,16 +20,16 @@ document.onload = (function () {
   }
 })();
 
-$(".switch__item").on("click", function() {
-    $(this).find('input:radio')[0].checked = true;
-    $('.js-radio-checked').toggleClass('js-radio-checked');
-    $(this).toggleClass('js-radio-checked');
+$(".switch__item").on("click", function () {
+  $(this).find('input:radio')[0].checked = true;
+  $('.js-radio-checked').toggleClass('js-radio-checked');
+  $(this).toggleClass('js-radio-checked');
 });
 
 //hamburger menu
-$('.main-menu__burger').click(function(){
+$('.main-menu__burger').click(function () {
   var itemList = document.querySelectorAll('.main-menu__item:not(.hidden)');
-  if($(this).hasClass('js-menu-opened')) {
+  if ($(this).hasClass('js-menu-opened')) {
     $(this).removeClass('js-menu-opened');
     $('.main-menu__list').slideUp(1000, function () {
       $(this).removeClass('js-menu-opened');
@@ -43,8 +43,8 @@ $('.main-menu__burger').click(function(){
 });
 
 //user menu
-$('.user-menu__toggle').click(function(){
-  if($(this).hasClass('js-user-opened')) {
+$('.user-menu__toggle').click(function () {
+  if ($(this).hasClass('js-user-opened')) {
     $(this).removeClass('js-user-opened');
     $('.user-menu__list').slideUp(1000, function () {
       $(this).removeClass('js-user-opened');
@@ -61,38 +61,38 @@ $('.user-menu__toggle').click(function(){
 var heroAccountElem = document.querySelector('.tour-accounts__text_hero');
 var superheroAccountElem = document.querySelector('.tour-accounts__text_superhero');
 
-if(heroAccountElem) {
+if (heroAccountElem) {
   heroAccountElem.addEventListener('mouseenter', function (event) {
     var heroLines = document.querySelectorAll('.collage__line_hero');
-    if(heroLines) {
-      for(var i = 0; i < heroLines.length; i++) {
+    if (heroLines) {
+      for (var i = 0; i < heroLines.length; i++) {
         heroLines[i].classList.toggle('js-line-active');
       }
     }
   }, false);
   heroAccountElem.addEventListener('mouseleave', function (event) {
     var heroLines = document.querySelectorAll('.collage__line_hero');
-    if(heroLines) {
-      for(var i = 0; i < heroLines.length; i++) {
+    if (heroLines) {
+      for (var i = 0; i < heroLines.length; i++) {
         heroLines[i].classList.toggle('js-line-active');
       }
     }
   }, false);
 }
 
-if(superheroAccountElem) {
+if (superheroAccountElem) {
   superheroAccountElem.addEventListener('mouseenter', function (event) {
     var superheroLines = document.querySelectorAll('.collage__line_superhero');
-    if(superheroLines) {
-      for(var i = 0; i < superheroLines.length; i++) {
+    if (superheroLines) {
+      for (var i = 0; i < superheroLines.length; i++) {
         superheroLines[i].classList.toggle('js-line-active');
       }
     }
   }, false);
   superheroAccountElem.addEventListener('mouseleave', function (event) {
     var superheroLines = document.querySelectorAll('.collage__line_superhero');
-    if(superheroLines) {
-      for(var i = 0; i < superheroLines.length; i++) {
+    if (superheroLines) {
+      for (var i = 0; i < superheroLines.length; i++) {
         superheroLines[i].classList.toggle('js-line-active');
       }
     }
@@ -100,14 +100,45 @@ if(superheroAccountElem) {
 }
 
 
-
 window.sr = ScrollReveal();
 
 //animation fall collage at tour.html page
-var tourCollageReveal1 = {delay: 100, distance: '-400px', easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)', scale: 1, mobile: true, duration: 1000, viewOffset: { right: -2000, left: -500}};
-var tourCollageReveal2 = {delay: 500, distance: '-400px', easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)', scale: 1, mobile: true, duration: 1000, viewOffset: { right: -2000, left: -500}};
-var tourCollageReveal3 = {delay: 1000, distance: '-400px', easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)', scale: 1, mobile: true, duration: 1000, viewOffset: { right: -2000, left: -500}};
-var tourCollageReveal4 = {delay: 1500, distance: '-400px', easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)', scale: 1, mobile: true, duration: 1000, viewOffset: { right: -2000, left: -500}};
+var tourCollageReveal1 = {
+  delay: 100,
+  distance: '-400px',
+  easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+  scale: 1,
+  mobile: true,
+  duration: 1000,
+  viewOffset: {right: -2000, left: -500}
+};
+var tourCollageReveal2 = {
+  delay: 500,
+  distance: '-400px',
+  easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+  scale: 1,
+  mobile: true,
+  duration: 1000,
+  viewOffset: {right: -2000, left: -500}
+};
+var tourCollageReveal3 = {
+  delay: 1000,
+  distance: '-400px',
+  easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+  scale: 1,
+  mobile: true,
+  duration: 1000,
+  viewOffset: {right: -2000, left: -500}
+};
+var tourCollageReveal4 = {
+  delay: 1500,
+  distance: '-400px',
+  easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+  scale: 1,
+  mobile: true,
+  duration: 1000,
+  viewOffset: {right: -2000, left: -500}
+};
 
 sr.reveal('.collage__img_resume1', tourCollageReveal1);
 sr.reveal('.collage__img_resume2', tourCollageReveal2);
@@ -115,17 +146,54 @@ sr.reveal('.collage__img_resume3', tourCollageReveal3);
 sr.reveal('.collage__img_resume4', tourCollageReveal4);
 
 //animation of the successive appearance blocks with resume at index.html page
-var mainResumesBlock1 = {delay: 100, distance: '500px', easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)', scale: 1, mobile: false, duration: 3000};
-var mainResumesBlock2 = {delay: 1000, distance: '500px', easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)', scale: 1, mobile: false, duration: 3000};
-var mainResumesBlock3 = {delay: 1500, distance: '500px', easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)', scale: 1, mobile: false, duration: 3000};
+var mainResumesBlock1 = {
+  delay: 100,
+  distance: '500px',
+  easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+  scale: 1,
+  mobile: false,
+  duration: 3000
+};
+var mainResumesBlock2 = {
+  delay: 1000,
+  distance: '500px',
+  easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+  scale: 1,
+  mobile: false,
+  duration: 3000
+};
+var mainResumesBlock3 = {
+  delay: 1500,
+  distance: '500px',
+  easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+  scale: 1,
+  mobile: false,
+  duration: 3000
+};
 
 sr.reveal('.resume-examples__img_1', mainResumesBlock2);
 sr.reveal('.resume-examples__img_2', mainResumesBlock1);
 sr.reveal('.resume-examples__img_3', mainResumesBlock3);
 
 //scroll animation
-var fooReveal1 = {delay: 100, distance: '100px', easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)', scale: 1, mobile: false, duration: 1000, viewOffset: { right: -2000, left: -1000, top: -1000}};
-var fooReveal2 = {delay: 200, distance: '100px', easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)', scale: 1, mobile: false, duration: 1000, viewOffset: { right: -2000, left: -1000, top: -1000}};
+var fooReveal1 = {
+  delay: 100,
+  distance: '100px',
+  easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+  scale: 1,
+  mobile: false,
+  duration: 1000,
+  viewOffset: {right: -2000, left: -1000, top: -1000}
+};
+var fooReveal2 = {
+  delay: 200,
+  distance: '100px',
+  easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+  scale: 1,
+  mobile: false,
+  duration: 1000,
+  viewOffset: {right: -2000, left: -1000, top: -1000}
+};
 
 sr.reveal('.page-section ', fooReveal1);
 sr.reveal('.text-block__title:not(.noreveal), .text-block__content:not(.noreveal) p, .text-block__link, .text-block__slogan, .text-block__item, .communication__img, .intro-video__item, .footer-menu__title:not(.noreveal), .footer-menu__item:not(.noreveal), .logo_footer:not(.noreveal), .pricing__header, .pricing__body, .pricing__footer:not(.noreveal), .note, .companies, .tour-working__img, .tour-accounts__img, .tour-dashboard__bg, .tour-search__bg, .tour-matching__img, .form__radiofield:not(.noreveal), .form__inputlabel:not(.noreveal), .about-slogan__text, .about-description__text, .about-description__comment, .about-partners__text, .about-partners__item, .about-team__title, .about-team__photo, .about-team__text, .about-donate__title, .about-donate__text, .about-donate__pics div, .video p, .video b, .page-section .btn, .video__youtube-content, .video__youtube-link, .video h2, .case h2, .case p, .case__inner-container, .versus-table h2, .versus-table p, .versus-table__t-row, .versus-table__r-row, .contacts__col, .contact__contact-link, .contacts__form-element, .news-detail h1, .news-category, .bread_crumbs, .images-block, .images-block h6, .images-block p, .images-block time, .text_page p, .text-bold p, .another-cathegory, .news_page, .follow', fooReveal2);
@@ -135,7 +203,7 @@ sr.reveal('.text-block__title:not(.noreveal), .text-block__content:not(.noreveal
 var heroBlock = document.querySelector('.lead__item_hero');
 var employerBlock = document.querySelector('.lead__item_employer');
 
-if(heroBlock && employerBlock) {
+if (heroBlock && employerBlock) {
   heroBlock.addEventListener('mouseenter', function (event) {
     heroBlock.classList.add('js-hero-hover-active');
     employerBlock.classList.add('js-employer-hover-none');
@@ -159,37 +227,37 @@ if(heroBlock && employerBlock) {
 
 //dropdown menu news-category
 
-$(function() {
+$(function () {
 
-    var dd = new DropDown( $('.news-category') );
+  var dd = new DropDown($('.news-category'));
 
-    $(document).click(function() {
-        // all dropdowns
-        $('.news-category').removeClass('activated');
-    });
+  $(document).click(function () {
+    // all dropdowns
+    $('.news-category').removeClass('activated');
+  });
 
 });
 
 //fixed top menu
 
-var StickyElement = function(node){
+var StickyElement = function (node) {
   var doc = $(document),
-      fixed = false,
-      anchor = node.find('.sticky__anchor'),
-      content = node.find('.sticky__content');
+    fixed = false,
+    anchor = node.find('.sticky__anchor'),
+    content = node.find('.sticky__content');
 
-  var onScroll = function(e){
+  var onScroll = function (e) {
     var docTop = doc.scrollTop(),
-        anchorTop = anchor.offset().top;
+      anchorTop = anchor.offset().top;
 
-    if(docTop > anchorTop){
-      if(!fixed){
+    if (docTop > anchorTop) {
+      if (!fixed) {
         anchor.height(content.outerHeight());
         content.addClass('fixed');
         fixed = true;
       }
-    }  else   {
-      if(fixed){
+    } else {
+      if (fixed) {
         anchor.height(0);
         content.removeClass('fixed');
         fixed = false;
@@ -200,9 +268,9 @@ var StickyElement = function(node){
   $(window).on('scroll', onScroll);
 };
 
-if(document.querySelector('.sticky')) {
+if (document.querySelector('.sticky')) {
 
-var demo = new StickyElement($('.sticky'));
+  var demo = new StickyElement($('.sticky'));
 }
 
 //Video at index page
@@ -211,15 +279,19 @@ var videoIndex, src, videoblockIndex;
 src = 'https://www.youtube.com/embed/';
 videoblockIndex = $(document).find('.intro-video');
 
-$(document).on("click", '.intro-video__btn', function(){
-	videoIndex = $(this).parents('.intro-video').data('video');
+$(document).on("click", '.intro-video__btn', function () {
+  videoIndex = $(this).parents('.intro-video').data('video');
   videoblockIndex.addClass('active'); // Добавляем активный класс, который увеличит блок по высоте и сделает видимым слой с видео
-  setTimeout(function(){ videoblockIndex.find('iframe').attr('src', src+videoIndex+'?autoplay=1'); }, 250);
+  setTimeout(function () {
+    videoblockIndex.find('iframe').attr('src', src + videoIndex + '?autoplay=1');
+  }, 250);
 });
 
-$(document).on("click", '.intro-video__close', function(){
-	videoblockIndex.removeClass('active');
-  setTimeout(function(){ videoblockIndex.find('iframe').attr('src', ''); }, 250);
+$(document).on("click", '.intro-video__close', function () {
+  videoblockIndex.removeClass('active');
+  setTimeout(function () {
+    videoblockIndex.find('iframe').attr('src', '');
+  }, 250);
 });
 
 //Video at vip page
@@ -228,21 +300,37 @@ var videoVip, videoblockVip;
 src = 'https://www.youtube.com/embed/';
 videoblockVip = $('.video__item');
 
-$('.video__youtube-link').click(function(event){
+$('.video__youtube-link').click(function (event) {
   event.preventDefault();
-	videoVip = $(this).parents('.video__item').data('video');
+  videoVip = $(this).parents('.video__item').data('video');
   videoblockVip.addClass('active'); // Добавляем активный класс, который увеличит блок по высоте и сделает видимым слой с видео
-  setTimeout(function(){ videoblockVip.find('iframe').attr('src', src+videoVip+'?autoplay=1'); }, 250);
+  setTimeout(function () {
+    videoblockVip.find('iframe').attr('src', src + videoVip + '?autoplay=1');
+  }, 250);
 });
 
-$('.lead__item_hero').hover(function(){
-  $(this).find("video")[0].play();
-}, function(){
-  $(this).find("video")[0].pause();
-});
+if ($(window).width() > 640) {
+  $('.lead__item_hero').hover(function () {
+    $(this).find("video")[0].play();
+  }, function () {
+    $(this).find("video")[0].pause();
+  });
 
-$('.lead__item_employer').hover(function(){
-  $(this).find("video")[0].play();
-}, function(){
-  $(this).find("video")[0].pause();
-});
+  $('.lead__item_employer').hover(function () {
+    $(this).find("video")[0].play();
+  }, function () {
+    $(this).find("video")[0].pause();
+  });
+} else {
+  $('.lead__item_hero').click(function () {
+    $(this).find("video")[0].play();
+  }, function () {
+    $(this).find("video")[0].pause();
+  });
+
+  $('.lead__item_employer').click(function () {
+    $(this).find("video")[0].play();
+  }, function () {
+    $(this).find("video")[0].pause();
+  });
+}
