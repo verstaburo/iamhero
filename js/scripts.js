28124df9bbe2a12da484066db59c4e723771e944
alege <aleggge@gmail.com>
2017-10-05 14:12:12 +0300

    burger fix, unused script removed

--- js/scripts.js
+++ js/scripts.js
@@ -301,38 +301,38 @@ $(function() {
 
   //fixed top menu
 
-  var StickyElement = function(node) {
-    var doc = $(document),
-      fixed = false,
-      anchor = node.find('.sticky__anchor'),
-      content = node.find('.sticky__content');
-
-    var onScroll = function(e) {
-      var docTop = doc.scrollTop(),
-        anchorTop = anchor.offset().top;
-
-      if (docTop > anchorTop) {
-        if (!fixed) {
-          anchor.height(content.outerHeight());
-          content.addClass('fixed');
-          fixed = true;
-        }
-      } else {
-        if (fixed) {
-          anchor.height(0);
-          content.removeClass('fixed');
-          fixed = false;
-        }
-      }
-    };
-
-    $(window).on('scroll', onScroll);
-  };
-
-  if (document.querySelector('.sticky')) {
-
-    var demo = new StickyElement($('.sticky'));
-  }
+  // var StickyElement = function(node) {
+  //   var doc = $(document),
+  //     fixed = false,
+  //     anchor = node.find('.sticky__anchor'),
+  //     content = node.find('.sticky__content');
+  //
+  //   var onScroll = function(e) {
+  //     var docTop = doc.scrollTop(),
+  //       anchorTop = anchor.offset().top;
+  //
+  //     if (docTop > anchorTop) {
+  //       if (!fixed) {
+  //         anchor.height(content.outerHeight());
+  //         content.addClass('fixed');
+  //         fixed = true;
+  //       }
+  //     } else {
+  //       if (fixed) {
+  //         anchor.height(0);
+  //         content.removeClass('fixed');
+  //         fixed = false;
+  //       }
+  //     }
+  //   };
+  //
+  //   $(window).on('scroll', onScroll);
+  // };
+  //
+  // if (document.querySelector('.sticky')) {
+  //
+  //   var demo = new StickyElement($('.sticky'));
+  // }
 
   //Video at index page
 
